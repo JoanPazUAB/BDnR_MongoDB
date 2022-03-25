a8ca9deefc1dcb05d6ccd6036df81544ee7208b7
Carla-Martinez <88989304+Carla-Martinez@users.noreply.github.com>
2022-03-25 23:46:00 +0100

    Update cases.py

--- cases.py
+++ cases.py
@@ -3,6 +3,7 @@
 from pydoc import doc
 from pymongo import MongoClient
 import json
+import options
 
 ################################## PARAMETRES DE CONNEXIÓ ###################################
 mongoUser = ''
@@ -68,33 +69,33 @@ CTID: 1
 
 # Parse options
 
+opts = Options()
+args = opts.parse()
 
-archivo = "cases.csv"
-
-#if args.fileName is not None:
-documents_patient = {}
-with open(archivo,"r") as fitxer:
-    fitxer.readline()
-    for line in fitxer:
-        line = line[:-1].split(';')
-        if line[0] not in documents_patient:
-            documents_patient[line[0]] = [{'PatientID': line[0],'Age':line[1], 'Gender': line[2], 'Diagnosis_Patient': line[4],
-                                          'Nodules': [{
-                                              'NoduleID': line[3], 
-                                              'Diagnosis_nodul': line[5], 
-                                              'Position':{'x':line[6], 'y': line[7], 'z':line[8]},
-                                              'Diameter': line[9],
-                                              'CTID': line[10]}] }, [line[3]]]
-        else:
-            if line[3] not in documents_patient[line[0]][1]:
-                nodule = {
-                    'NoduleID': line[3], 
-                    'Diagnosis_nodul': line[5], 
-                    'Position':{'x':line[6], 'y': line[7], 'z':line[8]},
-                    'Diameter': line[9],
-                    'CTID': line[10]}
-                documents_patient[line[0]][0]['Nodules'].append(nodule)
-                documents_patient[line[0]][1].append(line[3])
+if args.fileName is not None:
+     documents_patient = {}
+     with open(archivo,"r") as fitxer:
+         fitxer.readline()
+         for line in fitxer:
+             line = line[:-1].split(';')
+             if line[0] not in documents_patient:
+                 documents_patient[line[0]] = [{'PatientID': line[0],'Age':line[1], 'Gender': line[2], 'Diagnosis_Patient': line[4],
+                                               'Nodules': [{
+                                                   'NoduleID': line[3], 
+                                                   'Diagnosis_nodul': line[5], 
+                                                   'Position':{'x':line[6], 'y': line[7], 'z':line[8]},
+                                                   'Diameter': line[9],
+                                                   'CTID': line[10]}] }, [line[3]]]
+             else:
+                 if line[3] not in documents_patient[line[0]][1]:
+                     nodule = {
+                         'NoduleID': line[3], 
+                         'Diagnosis_nodul': line[5], 
+                         'Position':{'x':line[6], 'y': line[7], 'z':line[8]},
+                         'Diameter': line[9],
+                         'CTID': line[10]}
+                     documents_patient[line[0]][0]['Nodules'].append(nodule)
+                     documents_patient[line[0]][1].append(line[3])
                 
                                                       
 for x in documents_patient.values():    
